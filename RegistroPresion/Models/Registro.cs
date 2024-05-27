dc0cbae1c549ef1b27c6ed6ed3c594d10c31d715
noego <noego@VWNC51427>
2024-05-27 08:50:52 -0600

    Agregar archivos de proyecto.

---
+++ RegistroPresion/Models/Registro.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroPresion.Models
+{
+    [Table("registropresiones")]
+    public class Registro
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public byte Diastolica { get; set; }
+        [NotNull]
+        public byte Sistolica { get; set; }
+        [NotNull]
+        public byte Pulso { get; set; }
+        [NotNull]
+        public DateTime Fecha { get; set; }
+
+        [Ignore]
+        public string? Categoria { get; set; }
+    }
+}
